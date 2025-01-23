2fe98495dc9deaa107b6c2676882e7dd998ad7c8
Rob Gilliam <rob_gilliam@hotmail.com>
2025-01-23 19:23:42 +0000

    Reworked Cell, againgit status

--- cell.py
+++ cell.py
@@ -2,39 +2,47 @@ from point import Point
 from line import Line
 
 class Cell:
-    def __init__(self, x1, y1, x2, y2, win):
-        self.__win = win
+    def __init__(self, win):
+        self._win = win
+
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        self.centre = Point((x1 + x2) // 2, (y1 + y2) // 2)
 
         top_left = Point(x1, y1)
         top_right = Point(x2, y1)
         bottom_left = Point(x1, y2)
         bottom_right = Point(x2, y2)
 
-        self.walls = [
-            Line(top_left, top_right),
-            Line(top_right, bottom_right),
-            Line(bottom_left, bottom_right),
-            Line(top_left, bottom_left)
-        ]
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "black")
 
-        self.centre = Point((x1 + x2) / 2, (y1 + y2) / 2)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "black")
 
-    def add_top_exit(self):
-        self.walls[0] = None
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "black")
 
-    def add_right_exit(self):
-        self.walls[1] = None
-
-    def add_bottom_exit(self):
-        self.walls[2] = None
-
-    def add_left_exit(self):
-        self.walls[3] = None
-
-    def draw(self):
-        for wall in self.walls:
-            if wall:
-                self.__win.draw_line(wall, "black")
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "black")
 
     def draw_move(self, to_cell, undo=False):
-        self.__win.draw_line(Line(self.centre, to_cell.centre), "gray" if undo else "red")
+        self._win.draw_line(Line(self.centre, to_cell.centre), "gray" if undo else "red")
--- main.py
+++ main.py
@@ -4,31 +4,49 @@ from cell import Cell
 win = Window(800, 600)
 # Add some cells
 maze = [
-    #First row
+    #First column
     [
-        Cell(10, 10, 30, 30, win),
-        Cell(30, 10, 50, 30, win)
+        Cell(win),
+        Cell(win),
+        Cell(win)
     ],
-    #Second row
+    #Second column
     [
-        Cell(10, 30, 30, 50, win),
-        Cell(30, 30, 50, 50, win)
+        Cell(win),
+        Cell(win),
+        Cell(win)
     ]
 ]
-maze[0][0].add_bottom_exit()
-maze[0][0].add_right_exit()
-maze[0][1].add_left_exit()
-maze[1][0].add_top_exit()
-maze[1][0].add_right_exit()
-maze[1][1].add_left_exit()
-maze[1][1].add_bottom_exit()
 
-for cell in [c for r in maze for c in r]:
-    cell.draw()
+maze[0][0].has_right_wall = False
+maze[1][0].has_left_wall = False
 
-maze[0][0].draw_move(maze[0][1])
-maze[0][1].draw_move(maze[0][0], undo=True)
+maze[0][0].has_bottom_wall = False
+maze[0][1].has_top_wall = False
+
+maze[0][1].has_right_wall = False
+maze[1][1].has_left_wall = False
+
+maze[1][1].has_bottom_wall = False
+maze[1][2].has_top_wall = False
+
+maze[1][2].has_left_wall = False
+maze[0][2].has_right_wall = False
+
+maze[0][2].has_bottom_wall = False
+
+for i in range(2):
+    for j in range(3):
+        x1 = 10 + 50 * i
+        x2 = x1 + 50
+        y1 = 10 + 50 * j
+        y2 = y1 + 50
+        maze[i][j].draw(x1, y1, x2, y2)
+
 maze[0][0].draw_move(maze[1][0])
-maze[1][0].draw_move(maze[1][1])
+maze[1][0].draw_move(maze[0][0], undo=True)
+maze[0][0].draw_move(maze[0][1])
+maze[0][1].draw_move(maze[1][1])
+maze[1][1].draw_move(maze[1][2])
 
 win.wait_for_close()
