74d1b7c52b1181317ae590d67575f71acb1f42c0
Rob Gilliam <rob_gilliam@hotmail.com>
2025-01-24 21:12:03 +0000

    Add tests

--- cell.py
+++ cell.py
@@ -2,7 +2,7 @@ from point import Point
 from line import Line
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win = None):
         self._win = win
 
         self.has_left_wall = True
@@ -23,26 +23,28 @@ class Cell:
 
         self.centre = Point((x1 + x2) // 2, (y1 + y2) // 2)
 
-        top_left = Point(x1, y1)
-        top_right = Point(x2, y1)
-        bottom_left = Point(x1, y2)
-        bottom_right = Point(x2, y2)
+        if self._win:
+            top_left = Point(x1, y1)
+            top_right = Point(x2, y1)
+            bottom_left = Point(x1, y2)
+            bottom_right = Point(x2, y2)
 
-        if self.has_left_wall:
-            line = Line(Point(x1, y1), Point(x1, y2))
-            self._win.draw_line(line, "black")
+            if self.has_left_wall:
+                line = Line(top_left, bottom_left)
+                self._win.draw_line(line, "black")
 
-        if self.has_right_wall:
-            line = Line(Point(x2, y1), Point(x2, y2))
-            self._win.draw_line(line, "black")
+            if self.has_right_wall:
+                line = Line(top_right, bottom_right)
+                self._win.draw_line(line, "black")
 
-        if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line, "black")
+            if self.has_top_wall:
+                line = Line(top_left, top_right)
+                self._win.draw_line(line, "black")
 
-        if self.has_bottom_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line, "black")
+            if self.has_bottom_wall:
+                line = Line(bottom_left, bottom_right)
+                self._win.draw_line(line, "black")
 
     def draw_move(self, to_cell, undo=False):
-        self._win.draw_line(Line(self.centre, to_cell.centre), "gray" if undo else "red")
+        if self._win:
+            self._win.draw_line(Line(self.centre, to_cell.centre), "gray" if undo else "red")
--- maze.py
+++ maze.py
@@ -10,7 +10,7 @@ class Maze:
         num_cols,
         cell_size_x,
         cell_size_y,
-        win
+        win = None
     ):
         self._x1 = x1
         self._y1 = y1
@@ -39,5 +39,6 @@ class Maze:
         self._animate()
 
     def _animate(self):
-        self._win.redraw()
-        time.sleep(0.05)
+        if self._win:
+            self._win.redraw()
+            time.sleep(0.05)
